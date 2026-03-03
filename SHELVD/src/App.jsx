e729d6379080b3ae011f2ed3ae9a585ba4204c33
Umar <umar.muheed747@gmail.com>
2026-03-04 00:21:52 +1100

    adding a cdcard to the screen to test

--- SHELVD/src/App.jsx
+++ SHELVD/src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import CdCard from './components/CDCard'  
 
 function App() {
   return (
     <>
-      
+      <CdCard cd={{ title: "Dawn FM", artist: "The Weeknd", url: "https://via.placeholder.com/150" }} />
+      <CdCard cd={{ title: "After Hours", artist: "The Weeknd", url: "https://via.placeholder.com/150" }} />
+      <CdCard cd={{ title: "Hurry Up Tomorrow", artist: "The Weeknd", url: "https://via.placeholder.com/150" }} />
+      <CdCard cd={{ title: "Kissland", artist: "The Weeknd", url: "https://via.placeholder.com/150" }} />
     </>
   )
 }
