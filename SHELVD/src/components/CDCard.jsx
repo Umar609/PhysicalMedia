dc7c97aa213e516541a4b0ee801145db00461483
Umar <umar.muheed747@gmail.com>
2026-03-04 00:17:01 +1100

    created CdCard

---
+++ SHELVD/src/components/CDCard.jsx
@@ -0,0 +1,23 @@
+function CdCard({cd}) {
+
+    function onfavouriteClick() {
+       alert("Favourite button clicked!");
+    }
+
+  return <div className="cd-card">
+    <div className="cd-cover">
+        <img src={cd.url} alt={cd.title}/>
+        <div className="cd-overlay">
+            <button className="favourite-btn" onClick={onfavouriteClick}>
+                🤍
+            </button>
+        </div>
+    </div>
+    <div className="cd-info">
+        <h3>{cd.title}</h3>
+        <p>{cd.artist}</p>
+    </div>
+  </div>
+}
+
+export default CdCard;
