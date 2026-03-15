9a0020aeed67e01e1d66f29133b7a5420506417e
Umar <umar.muheed747@gmail.com>
2026-03-16 01:12:21 +1100

    adding app.jsx and adding fuinction on Home.jsx

---
+++ SHELVD/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+    const cds = [
+        {id: 1, title: "Dawn FM", artist: "The Weeknd", year: 2022},
+        {id: 2, title: "Hurry Up Tomorrow", artist: "The Weeknd", year: 2025},
+        {id: 3, title: "Casino", artist: "Baby Keem", year: 2026},
+        {id: 4, title: "The Melodic Blue", artist: "Baby Keem", year: 2024},
+    ];
+  return (
+    <div className="home">
+    </div>
+  );
+}
