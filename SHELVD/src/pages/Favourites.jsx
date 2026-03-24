c6fafbaaa7389990500a6ba6c03a4c772dd5fa50
Umar <umar.muheed747@gmail.com>
2026-03-25 00:15:53 +1100

    created favourites page

---
+++ SHELVD/src/pages/Favourites.jsx
@@ -0,0 +1,9 @@
+function Favourites() {
+  return (
+    <div className="favourites-empty">
+        <h2>Your favourites list is empty!</h2>
+        <p>Start adding your favourite media to see them here.</p>
+    </div>
+  )
+}
+export default Favourites;
