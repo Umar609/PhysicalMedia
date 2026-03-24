214f1541f6f26d168f706ca8604052b10fb54a51
Umar <umar.muheed747@gmail.com>
2026-03-25 00:24:22 +1100

    added fafvourites page import

--- SHELVD/src/App.jsx
+++ SHELVD/src/App.jsx
@@ -1,6 +1,6 @@
-import { Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
+import Favourites from './pages/Favourites'
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
