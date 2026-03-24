90ace1c510744bfcbd19d965ccb364be33f23d2e
Umar <umar.muheed747@gmail.com>
2026-03-25 00:05:06 +1100

    added search field, search button and states

--- SHELVD/src/pages/Home.jsx
+++ SHELVD/src/pages/Home.jsx
@@ -1,14 +1,34 @@
 import CdCard from "../components/CDCard";
+import {useState} from "react";
 
 function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+
     const cds = [
         {id: 1, title: "Dawn FM", artist: "The Weeknd", year: 2022},
         {id: 2, title: "Hurry Up Tomorrow", artist: "The Weeknd", year: 2025},
         {id: 3, title: "Casino", artist: "Baby Keem", year: 2026},
         {id: 4, title: "The Melodic Blue", artist: "Baby Keem", year: 2024},
     ];
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const query = event.target.elements.search.value;
+    alert(`Searching for: ${query}`);
+  }  
+
   return (
     <div className="home">
+        <form onSubmit={handleSearch} className="search-form">
+            <input 
+             type="text" 
+             placeholder="Search for a CD..." 
+             className="search-input"
+             value={searchQuery}
+             onChange={(e) => setSearchQuery(e.target.value)}
+             />
+            <button type="submit" className="search-button">Search</button>
+        </form>
         <div className="cd-grid">
             {cds.map(cd => <CdCard cd={cd} key={cd.id}/> )}
         </div>
