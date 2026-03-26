1895322309471a4f096d4ccdfeefe88255ded203
Umar <umar.muheed747@gmail.com>
2026-03-26 22:41:44 +1100

    added favourites grid

--- SHELVD/src/pages/Favourites.jsx
+++ SHELVD/src/pages/Favourites.jsx
@@ -1,6 +1,25 @@
 import '../css/Favorites.css';
+import { useCDContext } from '../contexts/CDContext';
+import CdCard from '../components/CDCard';
+
 
 function Favourites() {
+  const { favourites } = useCDContext();
+
+  if (favourites) {
+    return (
+    <div className="favourites">
+          <h2>Your Favourites</h2>
+      <div className="cd-grid">
+              {favourites.map((cd) => 
+                  (
+                  <CdCard cd={cd} key={cd.id}/>  
+                  ))}
+          </div>
+        </div>
+    );
+  }
+ 
   return (
     <div className="favourites-empty">
         <h2>Your favourites list is empty!</h2>
