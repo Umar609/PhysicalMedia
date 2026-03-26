d90b4e093ab93f33fb00c66e6b2a89363af34ee7
Umar <umar.muheed747@gmail.com>
2026-03-26 22:25:46 +1100

    added add., remove and check if favourites movie exists in favourites list

--- SHELVD/src/contexts/CDContext.jsx
+++ SHELVD/src/contexts/CDContext.jsx
@@ -1,7 +1,44 @@
-import { createContext, useState, useContext, useEffect } from "react";
+import {createContext, useState, useContext, useEffect} from "react";
 
 const CDContext = createContext();
 
 export const useCDContext  = () => useContext(CDContext);
 
-export const CDProvider = () => {}
+export const CDProvider = ({children}) => {
+    const [favourites, setFavourites] = useState([])
+    
+    useEffect(() => {
+        const storedFavs = localStorage.getItem("favourites")
+        
+        if (storedFavs) setFavourites(JSON.parse(storedFavs));
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem("favourites", JSON.stringify(favourites));
+    }, [favourites])
+
+    const addToFavourites = (cd) => {
+            setFavourites(prev => [...prev, cd]);
+        }
+
+    const removeFromFavourites = (cdId) => {
+        setFavourites(prev => prev.filter(cd => cd.id !== cdId));
+    }
+
+    const isFavourite = (cdId) => {
+        return favourites.some(cd => cd.id === cdId);
+    }
+
+    const value = {
+        favourites,
+        addToFavourites,
+        removeFromFavourites,
+        isFavourite
+    }
+
+    return (
+        <CDContext.Provider value={value}>
+            {children}
+        </CDContext.Provider>
+    );
+}
