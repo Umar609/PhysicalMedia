356638c616b484299c6970377e257c0069be2eac
Umar <umar.muheed747@gmail.com>
2026-03-26 21:20:49 +1100

    setup api key and base url as well as a getPopularMovies and searchMovies function

---
+++ SHELVD/src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY = "7413a3c569d56af9970f0824af9beb48";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+}
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+    return data.results;
+}
