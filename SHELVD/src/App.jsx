15e10325f410e2c5c1e14ab0f4cefaa065b77fe6
Umar <umar.muheed747@gmail.com>
2026-03-26 22:26:32 +1100

    wrapped in the cdprovidor

--- SHELVD/src/App.jsx
+++ SHELVD/src/App.jsx
@@ -3,18 +3,19 @@ import Home from './pages/Home'
 import Favourites from './pages/Favourites'
 import { Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
+import { CDProvider } from './contexts/CDContext'
 
 function App() {
   return (
-    <div>
-    <NavBar />
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favourites" element={<Favourites />} />
-      </Routes>
-    </main>
-    </div>
+    <CDProvider>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favourites" element={<Favourites />} />
+        </Routes>
+      </main>
+    </CDProvider>
   )
 }
 
