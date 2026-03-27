1e88cf712c86a261d04a8afc115eb59d4672e23e
Umar <umar.muheed747@gmail.com>
2026-03-27 17:20:29 +1100

    changewd all the cd card code to match the cd case code found on the internet
    changed from gridfe to a flex-wrap so cases work better

--- SHELVD/src/components/CDCard.jsx
+++ SHELVD/src/components/CDCard.jsx
@@ -1,34 +1,45 @@
 import '../css/MovieCard.css';
 import { useCDContext } from '../contexts/CDContext';
 
-
 function CdCard({cd}) {
     const {isFavourite, addToFavourites, removeFromFavourites} = useCDContext();
     const favourite = isFavourite(cd.id);
-        const imageSrc = cd.poster_path?.startsWith("http")
-            ? cd.poster_path
-            : `https://image.tmdb.org/t/p/w500${cd.poster_path}`;
+    const imageSrc = cd.poster_path?.startsWith("http")
+        ? cd.poster_path
+        : `https://image.tmdb.org/t/p/w500${cd.poster_path}`;
 
     function onfavouriteClick(e) {
-       e.preventDefault()
-       if (favourite) removeFromFavourites(cd.id)
-       else addToFavourites(cd)
+        e.preventDefault();
+        if (favourite) removeFromFavourites(cd.id);
+        else addToFavourites(cd);
     }
 
-  return <div className="cd-card">
-    <div className="cd-cover">
-        <img src={imageSrc} alt={cd.title}/>
-        <div className="cd-overlay">
-            <button className={`favourite-btn ${favourite ? "active" : ''}`} onClick={onfavouriteClick}>
+    return (
+        <div className="cd-case">
+            <div
+                className="album-art"
+                style={{ backgroundImage: `linear-gradient(rgba(255,255,255,0.15),rgba(255,255,255,0)), url(${imageSrc})` }}
+                title={cd.title}
+            >
+                <div className="sup pos-tl"></div>
+                <div className="sup pos-tr"></div>
+                <div className="sup pos-bl"></div>
+                <div className="sup pos-br"></div>
+            </div>
+            <div className="spine"></div>
+            <div className="cd-info">
+                <h3>{cd.title}</h3>
+                <p>{cd.artist}</p>
+            </div>
+            <button
+                className={`favourite-btn ${favourite ? 'active' : ''}`}
+                onClick={onfavouriteClick}
+                title={favourite ? 'Remove from favourites' : 'Add to favourites'}
+            >
                 ♥
             </button>
         </div>
-    </div>
-    <div className="cd-info">
-        <h3>{cd.title}</h3>
-        <p>{cd.artist}</p>
-    </div>
-  </div>
+    );
 }
 
 export default CdCard;
