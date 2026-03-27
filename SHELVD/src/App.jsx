0ac6b83e67e6787f09411560bd774ffc2da28708
Umar <umar.muheed747@gmail.com>
2026-03-27 18:09:46 +1100

    added route path for wishlist page function

--- SHELVD/src/App.jsx
+++ SHELVD/src/App.jsx
@@ -1,6 +1,7 @@
 import './css/App.css'
 import Home from './pages/Home'
 import Favourites from './pages/Favourites'
+import Wishlist from './pages/Wishlist'
 import { Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import { CDProvider } from './contexts/CDContext'
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/favourites" element={<Favourites />} />
+          <Route path="/wishlist" element={<Wishlist />} />
         </Routes>
       </main>
     </CDProvider>
