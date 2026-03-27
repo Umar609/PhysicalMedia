28646ee5b05962d06b85b9425f26e79d76551447
Umar <umar.muheed747@gmail.com>
2026-03-27 17:58:45 +1100

    added favourites code and changed all the variable to wishlist

---
+++ SHELVD/src/pages/Wishlist.jsx
@@ -0,0 +1,30 @@
+import '../css/Wishlist.css';
+import { useCDContext } from '../contexts/CDContext';
+import CdCard from '../components/CDCard';
+
+
+function Wishlist() {
+  const { wishlist } = useCDContext();
+
+  if (wishlist) {
+    return (
+    <div className="wishlist">
+          <h2>Your Wishlist</h2>
+      <div className="cd-grid">
+              {wishlist.map((cd) => 
+                  (
+                  <CdCard cd={cd} key={cd.id}/>  
+                  ))}
+          </div>
+        </div>
+    );
+  }
+ 
+  return (
+    <div className="wishlist-empty">
+        <h2>Your wishlist is empty!</h2>
+        <p>Start adding your favourite media to see them here.</p>
+    </div>
+  )
+}
+export default Wishlist;
