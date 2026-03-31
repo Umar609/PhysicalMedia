7996d3b7b7d077c8b75b4a36f1b9a80e4d001b34
Umar <umar.muheed747@gmail.com>
2026-03-31 22:01:48 +1100

    wrapped the form in a page wrapper

--- SHELVD/src/pages/LogIn.jsx
+++ SHELVD/src/pages/LogIn.jsx
@@ -2,19 +2,21 @@ import '../css/LogIn.css'
 
 function LogIn() {
   return (
-    <form className="form-control" action="">
-      <p className="title">Login</p>
-      <div className="input-field">
-        <input id="login-email" required className="input" type="text" />
-        <label className="label" htmlFor="login-email">Enter Email</label>
-      </div>
-      <div className="input-field">
-        <input id="login-password" required className="input" type="password" />
-        <label className="label" htmlFor="login-password">Enter Password</label>
-      </div>
-      <a href="#">Forgot your password?</a>
-      <button className="submit-btn" type="submit">Sign In</button>
-    </form>
+    <section className="login-page">
+      <form className="form-control" action="">
+        <p className="title">Login</p>
+        <div className="input-field">
+          <input id="login-email" required className="input" type="text" />
+          <label className="label" htmlFor="login-email">Enter Email</label>
+        </div>
+        <div className="input-field">
+          <input id="login-password" required className="input" type="password" />
+          <label className="label" htmlFor="login-password">Enter Password</label>
+        </div>
+        <a href="#">Forgot your password?</a>
+        <button className="submit-btn" type="submit">Sign In</button>
+      </form>
+    </section>
   )
 }
 
