df37329132e2ad826901126dfc934bdd40a16652
Umar <umar.muheed747@gmail.com>
2026-03-31 21:58:30 +1100

    added login route

--- SHELVD/src/App.jsx
+++ SHELVD/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import Owned from './pages/Owned'
 import Wishlist from './pages/Wishlist'
 import Profile from './pages/Profile'
+import LogIn from './pages/LogIn'
 import { Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import { CDProvider } from './contexts/CDContext'
@@ -17,6 +18,7 @@ function App() {
           <Route path="/owned" element={<Owned />} />
           <Route path="/wishlist" element={<Wishlist />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/Login" element={<LogIn />} />
         </Routes>
       </main>
     </CDProvider>
