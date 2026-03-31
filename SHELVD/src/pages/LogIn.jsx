15791ceb8202eae205f51ceca7ff251a58012cde
Umar <umar.muheed747@gmail.com>
2026-03-31 21:58:38 +1100

    added login function

--- SHELVD/src/pages/LogIn.jsx
+++ SHELVD/src/pages/LogIn.jsx
@@ -1,13 +1,21 @@
-<form class="form-control" action="">
-  <p class="title">Login</p>
-  <div class="input-field">
-    <input required="" class="input" type="text" />
-    <label class="label" for="input">Enter Email</label>
-  </div>
-  <div class="input-field">
-    <input required="" class="input" type="password" />
-    <label class="label" for="input">Enter Password</label>
-  </div>
-  <a>Forgot your password?</a>
-  <button class="submit-btn">Sign In</button>
-</form>
+import '../css/LogIn.css'
+
+function LogIn() {
+  return (
+    <form className="form-control" action="">
+      <p className="title">Login</p>
+      <div className="input-field">
+        <input id="login-email" required className="input" type="text" />
+        <label className="label" htmlFor="login-email">Enter Email</label>
+      </div>
+      <div className="input-field">
+        <input id="login-password" required className="input" type="password" />
+        <label className="label" htmlFor="login-password">Enter Password</label>
+      </div>
+      <a href="#">Forgot your password?</a>
+      <button className="submit-btn" type="submit">Sign In</button>
+    </form>
+  )
+}
+
+export default LogIn
