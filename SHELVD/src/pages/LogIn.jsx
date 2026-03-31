ba27a1f2cf91560b6830ed722629acfa72e471a0
Umar <umar.muheed747@gmail.com>
2026-03-31 21:49:05 +1100

    added login tile to log in page

---
+++ SHELVD/src/pages/LogIn.jsx
@@ -0,0 +1,13 @@
+<form class="form-control" action="">
+  <p class="title">Login</p>
+  <div class="input-field">
+    <input required="" class="input" type="text" />
+    <label class="label" for="input">Enter Email</label>
+  </div>
+  <div class="input-field">
+    <input required="" class="input" type="password" />
+    <label class="label" for="input">Enter Password</label>
+  </div>
+  <a>Forgot your password?</a>
+  <button class="submit-btn">Sign In</button>
+</form>
