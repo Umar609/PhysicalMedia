6233eea6fe39dc65369ef71459f3beba671a86a9
Umar <umar.muheed747@gmail.com>
2026-07-26 22:51:42 +1000

    adding account settings header in profile settings page

---
+++ parent/src/components/Users.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+const Users = () => {
+    const [users, setUsers] = useState([]);
+
+    return (
+        <article>
+            <h1>Users List</h1>  
+            {users?.length 
+            ? (
+                <ul>
+                    {users.map((user, i) => <li key={i}>{user?.username}</li>)}
+                </ul>
+            ) : <p>No users to display.</p>
+            }
+        </article>
+    );
+};
+
+export default Users;
--- parent/src/pages/Profile.jsx
+++ parent/src/pages/Profile.jsx
@@ -4,7 +4,9 @@ function Profile() {
     return (
         <div className="profile">
             <h2>User Profile</h2>
-            <p>This is where user profile information would be displayed.</p>
+            <p>This is where user profile information would be displayed.</p> 
+            <h2>Account Settings</h2>
+            <p>This is where account settings would be managed.</p>  
         </div>
     );
 }
