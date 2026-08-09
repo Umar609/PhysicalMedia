b81007edb45066d176f7bcd94dba528c0cd7960c
Umar <umar.muheed747@gmail.com>
2026-08-09 14:24:09 +1000

    added text fields for the account settings and also rearraanged them and made them look nice

--- parent/src/pages/Profile.jsx
+++ parent/src/pages/Profile.jsx
@@ -5,12 +5,48 @@ function Profile() {
         <div className="profile">
             <h2>Account Settings</h2>
             <h4>Username</h4>
-            <h4>First Name</h4>
-            <h4>Last Name</h4>
-            <h4>Email address</h4>
-            <h4>Location</h4>
+            <div className="input-field">
+                <input id="username" required className="input" type="text" />
+                <label className="label" htmlFor="username">Enter Username</label>
+            </div>
+            <div className="profile-row">
+                <div>
+                    <h4>First Name</h4>
+                    <div className="input-field">
+                        <input id="first-name" required className="input" type="text" />
+                        <label className="label" htmlFor="first-name">Enter First Name</label>
+                    </div>
+                </div>
+                <div>
+                    <h4>Last Name</h4>
+                    <div className="input-field">
+                        <input id="last-name" required className="input" type="text" />
+                        <label className="label" htmlFor="last-name">Enter Last Name</label>
+                    </div>
+                </div>
+            </div>
+            <div className="profile-row">
+                <div>
+                    <h4>Email address</h4>
+                    <div className="input-field">
+                        <input id="email" required className="input" type="email" />
+                        <label className="label" htmlFor="email">Enter Email</label>
+                    </div>
+                </div>
+                <div>
+                    <h4>Location</h4>
+                    <div className="input-field">
+                        <input id="location" required className="input" type="text" />
+                        <label className="label" htmlFor="location">Enter Location</label>
+                    </div>
+                </div>
+            </div>
             <h4>Bio</h4>
-            <p>Save changes button would go here.</p>
+            <div className="input-field">
+                <textarea id="bio" required className="input" />
+                <label className="label" htmlFor="bio">Enter Bio</label>
+            </div>
+            <button className="submit-btn" type="submit">Save Changes</button>
         </div>
     );
 }
