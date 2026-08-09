529d4d5aa6d374eb53fcd1c0320fdec3964365cb
Umar <umar.muheed747@gmail.com>
2026-08-09 12:58:22 +0900

    added the sub titles for the settings page

--- parent/src/pages/Profile.jsx
+++ parent/src/pages/Profile.jsx
@@ -3,10 +3,14 @@ import '../css/Profile.css';
 function Profile() {
     return (
         <div className="profile">
-            <h2>User Profile</h2>
-            <p>This is where user profile information would be displayed.</p> 
             <h2>Account Settings</h2>
-            <p>This is where account settings would be managed.</p>  
+            <h4>Username</h4>
+            <h4>First Name</h4>
+            <h4>Last Name</h4>
+            <h4>Email address</h4>
+            <h4>Location</h4>
+            <h4>Bio</h4>
+            <p>Save changes button would go here.</p>
         </div>
     );
 }
